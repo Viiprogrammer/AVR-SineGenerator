23b5e6a7e32eb5b0f951b5e3e096459902463bc6
Maxim Telegin <m_telega@mail.ru>
2021-04-09 01:11:39 +0300

    Init AtmelStudio project

---
+++ SineGeneratorProject/SineGenerator/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SineGenerator.cpp
+ *
+ * Created: 09.04.2021 1:09:32
+ * Author : Maxim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
